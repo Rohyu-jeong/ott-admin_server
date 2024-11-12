79764cf311a478ef6a94732e53311f001562d231
Rohyj <yacheku@gmail.com>
2024-11-12 10:43:22 +0900

    app.ts port 추가

--- app.ts
+++ app.ts
@@ -5,9 +5,13 @@ import router from "./routers/router";
 import './db';
 
 export const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/image', express.static(path.join(__dirname, '../OTT_WEB-client/public/image')));
